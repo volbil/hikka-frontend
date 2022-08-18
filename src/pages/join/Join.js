3adb67bb9569083b90956e482996163ac9785923
volbil <volbilnexus@gmail.com>
2022-08-18 20:34:51 +0300

    Join form

--- src/pages/join/Join.js
+++ src/pages/join/Join.js
@@ -1,136 +1,121 @@
+import { useForm } from 'react-hook-form'
 import { Center } from '@chakra-ui/react'
 import { Button } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
 import { useState } from 'react'
+import { useRef } from 'react'
 import axios from 'axios'
 
-// import ErrorText from '../../components/common/ErrorText'
-// import apiJoin from '../../requests'
 
 const Join = () => {
   const [loading, setLoading] = useState(false)
 
-  const [values, setValues] = useState({
-    username: '', email: '', password: '', passwordConfirm: ''
-  })
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    watch
+  } = useForm()
 
-  const [valid, setValuesValid] = useState({
-    username: true, email: true, password: true, passwordConfirm: true
-  })
+  const password = useRef({});
+  password.current = watch('password', '')
 
-  const setValue = name => {
-    return ({ target: { value } }) => {
-      setValues(oldValues => ({...oldValues, [name]: value }));
-    }
-  }
-
-  const setValid = (name, valid) => {
-    setValuesValid(oldValues => ({...oldValues, [name]: valid }));
-  }
-
-  const doJoin = async (e) => {
-    e.preventDefault()
-
-    setLoading(true)
-
-    if (values.password < 6)
-      setValid('username', false)
-
-    const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (regex.test(values.email) === false)
-      setValid('email', false)
-
-    if (values.password < 9)
-      setValid('password', false)
-
-    if (values.passwordConfirm !== values.password)
-      setValid('passwordConfirm', false)
-
-    if (valid.username && valid.email && valid.password && valid.passwordConfirm) {
-      await axios.post('https://api.hikka.io/auth/join', {
-        'username': values.username,
-        'email': values.email,
-        'password': values.password
-      })
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function ({ response }) {
-          console.log(response.data);
-        })
-    }
-
-    setLoading(false)
+  function onSubmit(values) {
+    console.log(values)
   }
 
   return (
     <Center>
-      <Box p={5} m={10} w="720px" borderWidth="1px" borderRadius="3px">
-        <Box mb={5}>
-          <Input
-            errorBorderColor='red.300'
-            placeholder='Username'
-            type='text'
-            onChange={setValue('username')}
-            isInvalid={!valid.username}
-          />
-          {/* <ErrorText invalid={!valid.username}>
-            Username must be at least 6 characters long
-          </ErrorText> */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box p={5} m={10} w="720px" borderWidth="1px" borderRadius="3px">
+          <Box mb={5}>
+            <Input
+              errorBorderColor='red.300'
+              placeholder='Username'
+              type='text'
+              {...register('username', {
+                required: 'Username is required',
+                minLength: { value: 6, message: 'Username must be at least 6 characters long' },
+              })}
+              isInvalid={errors.username}
+            />
+            <Text
+              mt={1} display={!errors.username ? 'none' : 'block'}
+              fontWeight='semibold'
+              color='red.300'
+            >{errors.username && errors.username.message}</Text>
+          </Box>
+
+
+          <Box mb={5}>
+            <Input
+              errorBorderColor='red.300'
+              placeholder='Email'
+              type='email'
+              {...register('email', {
+                required: 'Email is required',
+                pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+              })}
+              isInvalid={errors.email}
+            />
+            <Text
+              mt={1} display={!errors.email ? 'none' : 'block'}
+              fontWeight='semibold'
+              color='red.300'
+            >{errors.email && errors.email.message}</Text>
+          </Box>
+
+
+          <Box mb={5}>
+            <Input
+              errorBorderColor='red.300'
+              placeholder='Password'
+              type='password'
+              {...register('password', {
+                required: 'Password is required',
+                minLength: { value: 8, message: 'Password must be at least 8 characters long' },
+              })}
+              isInvalid={errors.password}
+            />
+            <Text
+              mt={1} display={!errors.password ? 'none' : 'block'}
+              fontWeight='semibold'
+              color='red.300'
+            >{errors.password && errors.password.message}</Text>
+          </Box>
+
+
+          <Box mb={5}>
+            <Input
+              errorBorderColor='red.300'
+              placeholder='Confirm password'
+              type='password'
+              {...register('passwordConfirm', {
+                required: 'Confirm password is required',
+                validate: value =>
+                  value === password.current || "The passwords do not match"
+              })}
+              isInvalid={errors.passwordConfirm}
+            />
+            <Text
+              mt={1} display={!errors.passwordConfirm ? 'none' : 'block'}
+              fontWeight='semibold'
+              color='red.300'
+            >{errors.passwordConfirm && errors.passwordConfirm.message}</Text>
+          </Box>
+
+
+          <Button
+            isLoading={loading}
+            w="100%" colorScheme='blue'
+            type='submit'
+          >
+            Submit
+          </Button>
         </Box>
-
-
-        <Box mb={5}>
-          <Input
-            errorBorderColor='red.300'
-            placeholder='Email'
-            type='email'
-            onChange={setValue('email')}
-            isInvalid={!valid.email}
-          />
-          {/* <ErrorText invalid={!valid.email}>
-            Invalid email
-          </ErrorText> */}
-        </Box>
-
-
-        <Box mb={5}>
-          <Input
-            errorBorderColor='red.300'
-            placeholder='Password'
-            type='password'
-            onChange={setValue('password')}
-            isInvalid={!valid.password}
-          />
-          {/* <ErrorText invalid={!valid.password}>
-            Password must be at least 8 characters long
-          </ErrorText> */}
-        </Box>
-
-
-        <Box mb={5}>
-          <Input
-            errorBorderColor='red.300'
-            placeholder='Confirm password'
-            type='password'
-            onChange={setValue('passwordConfirm')}
-            isInvalid={!valid.passwordConfirm}
-          />
-          {/* <ErrorText invalid={!valid.passwordConfirm}>
-            Passwords don't match
-          </ErrorText> */}
-        </Box>
-
-
-        <Button
-          isLoading={loading}
-          w="100%" colorScheme='blue'
-          onClick={(e) => doJoin(e)}
-        >
-          Submit
-        </Button>
-      </Box>
+      </form>
     </Center>
   )
 }
